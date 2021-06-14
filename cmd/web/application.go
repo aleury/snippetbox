7450943fade0d3d51028d786590971429d7436a1
Adam Eury <adam@coherentpath.com>
2021-06-14 06:02:01 -0400

    Update app.serverError

--- cmd/web/application.go
+++ cmd/web/application.go
@@ -56,7 +56,8 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
 
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	status := http.StatusInternalServerError
+	http.Error(w, http.StatusText(status), status)
 }
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
