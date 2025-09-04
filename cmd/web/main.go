880b62c7fef0d1a3058cc419bb85fc4271ec314b
Adam Eury <euryadam@gmail.com>
2025-09-04 08:54:32 -0400

    Structured logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	mux := http.NewServeMux()
 
@@ -20,6 +23,9 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("starting server on %s\n", *addr)
-	log.Fatal(http.ListenAndServe(*addr, mux))
+	logger.Info("starting server", "addr", *addr)
+	if err := http.ListenAndServe(*addr, mux); err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 }
