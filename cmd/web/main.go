f4af0b9e9c0f79170c723d7ad38b44c18473a614
Adam Eury <euryadam@gmail.com>
2025-09-06 18:14:10 -0400

    Isolate application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,23 +15,14 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	mux := http.NewServeMux()
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	app := application{
 		logger: logger,
 	}
 
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("starting server", "addr", *addr)
-	if err := http.ListenAndServe(*addr, mux); err != nil {
+	if err := http.ListenAndServe(*addr, app.routes()); err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
